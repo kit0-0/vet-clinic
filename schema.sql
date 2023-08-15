cd6dc3bf6de73e36225482cb9a39f8405ac97203
kit0-0 <kt1767@gmail.com>
2023-08-15 21:01:40 +0530

    Create and modify specues and owner table

--- schema.sql
+++ schema.sql
@@ -9,3 +9,23 @@ CREATE TABLE animals (
 
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(255);
+
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(255),
+    age INTEGER
+);
+
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+ALTER TABLE animals
+    DROP COLUMN species,
+    ADD COLUMN species_id INTEGER,
+    ADD COLUMN owner_id INTEGER;
+
+ALTER TABLE animals
+    ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species(id),
+    ADD CONSTRAINT fk_owner FOREIGN KEY (owner_id) REFERENCES owners(id);
