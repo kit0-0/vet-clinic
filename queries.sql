55b283455c9e95f7083729b97b623b73b948fe3c
kit0-0 <kt1767@gmail.com>
2023-08-21 15:00:46 +0530

    Update query

--- queries.sql
+++ queries.sql
@@ -10,12 +10,15 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 
 SELECT * FROM animals WHERE neutered = TRUE;
 
+SELECT * FROM animals WHERE name != 'Gabumon';
+
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
 BEGIN;
 UPDATE animals SET species = 'Unspecified';
 SELECT * FROM animals;
 ROLLBACK;
+SELECT * FROM animals;
 
 START TRANSACTION;
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
